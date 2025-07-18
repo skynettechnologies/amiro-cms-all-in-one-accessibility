0ce257d307167b31ec6287bf9a1618d29126eafb
iOS Developer <developer3@skynettechnologies.com>
2025-07-18 12:05:45 +0530

    code updated.

--- database/uninstall.sql
+++ database/uninstall.sql
@@ -1,2 +1,2 @@
 UPDATE `cms_layouts`
-SET `html_head_tail` = REPLACE(`html_head_tail`, '<script id="aioa-adawidget" src="https://www.skynettechnologies.com/accessibility/js/all-in-one-accessibility-js-widget-minify.js?colorcode=#420083&token=ADAAIOA-FDFA7781E59B83725BDF12926F04414D&position=bottom_right" defer></script>', '');
+SET `html_head_tail` = REPLACE(`html_head_tail`, '<script id="aioa-adawidget" src="https://www.skynettechnologies.com/accessibility/js/all-in-one-accessibility-js-widget-minify.js?colorcode=#420083&token=&position=bottom_right" defer></script>', '');
